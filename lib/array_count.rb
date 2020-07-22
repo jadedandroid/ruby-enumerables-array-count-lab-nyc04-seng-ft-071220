56e7f67a6ed54b78bc00d702ad01399c956664f9
jade decordova <lacroixjaded@gmail.com>
2020-07-22 16:09:00 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,4 +1,19 @@
+require "pry"
+
 def count_strings(array)
+  all_string = []
+  string = 
+  array.count{| element | 
+  if element == String do 
+  all_string << element 
+  
+  end
+  
+end
+  }
+  return all_string.count
+  
+  binding.pry
   # Return the total number of strings in the provided array using the count enumerable
 end
 
